c77afedae06df05c30d912b3b824b8f0213c8400
Ilnaz Huzin <huzin.dds@yandex.ru>
2023-12-07 20:44:27 +0300

    Пу-пу-пуу

---
+++ courses/Controllers/CoursesController.cs
@@ -0,0 +1,19 @@
+﻿using courses.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace courses.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext context;
+        CoursesController(ApplicationDbContext context) {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            var courses = context.Courses.ToList();
+            return View(courses);
+        }
+    }
+}
